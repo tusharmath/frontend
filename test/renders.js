8b58a6b1318b4d9c2fa3991e8a050abda2d64859
Carlos Derich <d.erich@hotmail.com>
2013-01-08 12:39:29 -0200

    rendering mocks

--- test/renders.js
+++ test/renders.js
@@ -3,15 +3,6 @@ var assert = require('assert')
    ,should = require('should')
 
 
-var request = {};
-var response = {
-    viewName: ""
-    , data : {}
-    , render: function(view, viewData) {
-        this.viewName = view;
-        this.data = viewData;
-    }
-};
 describe("Default Route", function(){
     it("should provide the a title and the index view name", function(done){
         routes.home({}, {
