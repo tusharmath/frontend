8d8ce6511b7cda1fc900b86ca8f08ff6be44465a
Carlos Derich <d.erich@hotmail.com>
2013-01-11 18:20:09 -0200

    render test

--- test/renders.js
+++ test/renders.js
@@ -3,8 +3,8 @@ var assert = require('assert')
    ,should = require('should')
 
 
-describe("Default Route", function(){
-    it("should provide the a title and the index view name", function(done){
+describe("Default Routes Rendering", function(){
+    it("should provide a title and the index Home name", function(done){
         routes.home({}, {
             render: function (viewName) {
                 viewName.should.equal("home")
@@ -16,6 +16,30 @@ describe("Default Route", function(){
         routes.about({}, {
             render: function (viewName) {
                 viewName.should.equal("about")
+                done()
+            }
+        })
+    })
+    it("should provide the a title and the leisure view name", function(done){
+        routes.leisure({}, {
+            render: function (viewName) {
+                viewName.should.equal("leisure")
+                done()
+            }
+        })
+    })
+    it("should provide the a title and the business view name", function(done){
+        routes.business({}, {
+            render: function (viewName) {
+                viewName.should.equal("business")
+                done()
+            }
+        })
+    })
+    it("should provide the a title and the business view name", function(done){
+        routes.terms({}, {
+            render: function (viewName) {
+                viewName.should.equal("terms")
                 done()
             }
         })
